654da3c5d3e72ee83fb8289f5fba87ce1f3d6261
nadlaraditya <nadlaraditya13@gmail.com>
2025-02-21 08:13:22 +0700

    membuat navbar

---
+++ index.js
@@ -0,0 +1,52 @@
+const navIcon = document.querySelector('.nav-icon');
+const overlay = document.querySelector('.overlay');
+const hero = document.querySelector('.hero');
+const heroContent = document.querySelector('.hero-content');
+const menuItems = gsap.utils.toArray('ul li a');
+
+let menuOpen = false;
+
+gsap.set(overlay, {
+    scaleY: 0,
+});
+gsap.set(menuItems, {
+    yPercent: 100
+});
+
+const tl = gsap.timeline({
+    default: {
+        ease: 'power4.inOut',
+        duration: 1
+    }
+});
+
+tl
+.to(hero, {
+    scale: 8.95,
+})
+.to(heroContent, {
+    pointerEvents: 'none',
+    filter: 'blur(4px)'
+}, "<")
+.to(overlay, {
+    scaleY: 1
+}, "-=.5")
+.to(navIcon, {
+    color: '#fff',
+}, "-=.9")
+.to(menuItems, {
+    yPercent: 0,
+    stragger: 0.1,
+    duration: 2
+}, "-=1.5")
+tl.pause();
+
+navIcon.addEventListener('click', () => {
+    if (!menuOpen) {
+        tl.play();
+        menuOpen = true;
+    } else{
+        tl.reverse();
+        menuOpen = false;
+    }
+});
